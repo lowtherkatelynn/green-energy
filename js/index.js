c99eb60ed90ffe394ba05ce834d4df041fa9951a
Katelynn Lowther <39056972+lowtherkatelynn@users.noreply.github.com>
2018-07-16 13:06:22 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -22,17 +22,17 @@ const container = document.getElementsByClassName("content");
 let contents = {
     sun: {
         title: "The Sun Yo",
-        url: "../img/sun.png",
+        url: "./img/sun.png",
         bodyTxt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget odio purus. Sed vulputate sem vitae mi finibus, eu venenatis ipsum viverra. Curabitur augue ex, feugiat in bibendum fermentum, imperdiet vitae elit. Vivamus arcu metus, faucibus id iaculis non, commodo eu velit. Curabitur interdum eleifend eros, vitae auctor leo interdum nec. Duis sagittis risus id turpis placerat, non congue ex dignissim. Aliquam tempor fringilla tellus, sit amet vestibulum lacus efficitur in."
     },
     recycle: {
         title: "Pls Recycle",
-        url: "../img/recycle.png",
+        url: "./img/recycle.png",
         bodyTxt: "Phasellus mollis nulla nunc, in luctus leo ultrices nec. Nullam suscipit dui lectus, a tristique sapien gravida sed. Nulla auctor nisl ac consectetur varius. In pulvinar varius urna eget mattis. Mauris in feugiat nibh. Pellentesque pellentesque posuere ante sit amet rutrum. Phasellus commodo fringilla gravida. Integer nunc velit, ullamcorper ut risus in, dignissim eleifend felis."
     },
     windmill: {
         title: "They're neat",
-        url: "../img/windmill.png",
+        url: "./img/windmill.png",
         bodyTxt: "Nullam cursus quam vitae augue vulputate, eu fringilla justo faucibus. Aliquam ut viverra sapien, ac accumsan arcu. Vivamus suscipit maximus dolor, nec eleifend metus sodales in. Cras iaculis porta facilisis. Etiam malesuada aliquam neque, et luctus velit lobortis quis. Aenean est enim, tincidunt eu felis in, sodales vestibulum mauris. Integer laoreet a quam ut commodo. Cras vitae placerat eros. Duis eu neque dolor. In rhoncus aliquet mi, feugiat varius massa commodo nec."
     }
 }
